05ae68100703adb3fd0076a96badaa8f4d74222f
zerox <jankodjuric0x@gmail.com>
2021-09-05 21:59:39 +0200

    Initialize empty repository

---
+++ src/main/java/com/janko/expertise/DatastoreLoader/DatastoreLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.janko.expertise.DatastoreLoader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatastoreLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatastoreLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janko/expertise/DatastoreLoader/DatastoreLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janko.expertise.DatastoreLoader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatastoreLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
