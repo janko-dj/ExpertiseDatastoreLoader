a9a9fb28d6272d052887faffe51083651c085ba0
zerox <jankodjuric0x@gmail.com>
2021-09-06 17:45:37 +0200

    Remove unused imports

--- src/main/java/com/janko/expertise/DatastoreLoader/loader/BigQueryDataRetriever.java
+++ src/main/java/com/janko/expertise/DatastoreLoader/loader/BigQueryDataRetriever.java
@@ -15,7 +15,6 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 @Component
 public class BigQueryDataRetriever {
